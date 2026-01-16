c54e58b7278ad89cfb9b4d79fec25d9232861b3d
Rofiyanto Bagus P. <pradiktabagus@gmail.com>
2026-01-16 16:35:52 +0700

    [TASK] Improvement Clean Architecture Fixes #25

--- src/shared/api/contract/bills-to-pay/client.ts
+++ src/shared/api/contract/bills-to-pay/client.ts
@@ -2,6 +2,6 @@ import { BillsToPayApiResponse } from "./types";
 import { api } from "@/shared/api/axios";
 
 export const getBillsToPay = async (): Promise<BillsToPayApiResponse> => {
-  const response = await api.get("/api/settlements/bills-to-pay");
+  const response = await api.get("/settlements/bills-to-pay");
   return response.data;
 };
